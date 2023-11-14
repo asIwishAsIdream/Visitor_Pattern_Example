57b89929cb683ee8b9ddf50b3421ac06cd9fc574
choinakhyeon <109520949+asIwishAsIdream@users.noreply.github.com>
2023-11-14 22:57:44 +0900

    Create Monitor.java

---
+++ src/ex1/diagnosis/Cars/Monitor.java
@@ -0,0 +1,18 @@
+
+
+
+public class Monitor {
+  
+  public void getStatus(CarElement carElement) {
+     carElement.getStatus()
+  } 
+}
+
+
+
+public class Diagnosis {
+  
+  public void getProblem(CarElement carElement) {
+     carElement.getgetProblem()
+  } 
+}
