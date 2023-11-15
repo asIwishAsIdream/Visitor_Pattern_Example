f955770fc50215460299365e71fe535c645a6ba1
choinakhyeon <109520949+asIwishAsIdream@users.noreply.github.com>
2023-11-16 08:25:19 +0900

    Update Client.java

--- src/ex1/diagnosis/Cars/Client.java
+++ src/ex1/diagnosis/Cars/Client.java
@@ -4,25 +4,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Client {
-  CarElement engine = new Engine();
-  CarElement lights = new Lights();
-  CarElement tire = new Tire();
+    List<Object> elements = Array.asList{
+        new Engine(),
+        new Lights(),
+        new Tire();
+    }
 
-  Monitor monitor = new Monitor();
-  
-  // 상태(Status)를 체크하는 elements 리스트 생성
-  List<Object> statusElements = Arrays.asList(
-      monitor.getStatus(engine),
-      monitor.getStatus(lights),
-      monitor.getStatus(tire)
-  );
-  
-  // 문제(Diagnosis)를 체크하는 elements 리스트 생성
-  Diagnosis diagnosis = new Diagnosis();
-  
-  List<Object> problemElements = Arrays.asList(
-      monitor.getProblem(engine),
-      monitor.getProblem(lights),
-      monitor.getProblem(tire)
-  );
+    Monitor monitor = new Monitor();
+    for( CarElement element : elements )
+    {
+        monitor.getStatus(element);
+    }
 }
